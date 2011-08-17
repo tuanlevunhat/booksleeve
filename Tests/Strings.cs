054ad8adb83ad3a4e439afb527aee77b8338ac40
mgravell <mgravell@localhost>
2011-08-17 13:02:43 +0100

    test Apend

--- Tests/Strings.cs
+++ Tests/Strings.cs
@@ -5,5 +5,25 @@ namespace Tests
     [TestFixture]
     public class Strings // http://redis.io/commands#string
     {
+        [Test]
+        public void Append()
+        {
+            using(var conn = Config.GetUnsecuredConnection())
+            {
+                conn.Keys.Remove(2, "append");
+                var s0 = conn.Strings.GetString(2, "append");
+
+                conn.Strings.Set(2, "append", "abc");
+                var s1 = conn.Strings.GetString(2, "append");
+
+                var result = conn.Strings.Append(2, "append", "defgh");
+                var s3 = conn.Strings.GetString(2, "append");
+
+                Assert.AreEqual(null, conn.Wait(s0));
+                Assert.AreEqual("abc", conn.Wait(s1));
+                Assert.AreEqual(8, conn.Wait(result));
+                Assert.AreEqual("abcdefgh", conn.Wait(s3));
+            }
+        }
     }
 }
