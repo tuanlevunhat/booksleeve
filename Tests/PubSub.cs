6b90196d690266d8cbeac10b3262905d8478a9d9
mgravell <mgravell@localhost>
2012-08-02 07:50:52 +0100

    ADD: timeout tracking; FIX: event unsubscription tweak

--- Tests/PubSub.cs
+++ Tests/PubSub.cs
@@ -86,5 +86,36 @@ namespace Tests
                 Assert.AreEqual(1, Interlocked.CompareExchange(ref gotB, 0, 0));
             }
         }
+
+        [Test]
+        public void TestSubscribeUnsubscribeAndSubscribeAgain()
+        {
+            using(var pub = Config.GetUnsecuredConnection())
+            using(var sub = Config.GetSubscriberConnection())
+            {
+                int x = 0, y = 0;
+                sub.Subscribe("abc", delegate { Interlocked.Increment(ref x); });
+                sub.PatternSubscribe("ab*", delegate { Interlocked.Increment(ref y); });
+                Thread.Sleep(50);
+                pub.Publish("abc", "");
+                Thread.Sleep(50);
+                Assert.AreEqual(1, Thread.VolatileRead(ref x));
+                Assert.AreEqual(1, Thread.VolatileRead(ref y));
+                sub.Unsubscribe("abc");
+                sub.PatternUnsubscribe("ab*");
+                Thread.Sleep(50);
+                pub.Publish("abc", "");
+                Assert.AreEqual(1, Thread.VolatileRead(ref x));
+                Assert.AreEqual(1, Thread.VolatileRead(ref y));
+                sub.Subscribe("abc", delegate { Interlocked.Increment(ref x); });
+                sub.PatternSubscribe("ab*", delegate { Interlocked.Increment(ref y); });
+                Thread.Sleep(50);
+                pub.Publish("abc", "");
+                Thread.Sleep(50);
+                Assert.AreEqual(2, Thread.VolatileRead(ref x));
+                Assert.AreEqual(2, Thread.VolatileRead(ref y));
+                
+            }
+        }
     }
 }
